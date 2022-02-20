7bd9c570d2f6af37686a056c6fba9c0ff071938c
Bruno Moureau <bruno.moureau@hotmail.com>
2022-02-20 15:25:30 +0100

    added some NavigationController tests

---
+++ Xamarin.Basics.UnitTests/Mvvm/Navigations/Controllers/NavigationControllerTests.can_load_views.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using Xamarin.Basics.Mvvm.Navigations;
+using Xamarin.Basics.Mvvm.Navigations.Controllers;
+using Xamarin.Basics.Mvvm.Views;
+using Xamarin.Basics.Tests.Helpers.Statics;
+using Xunit;
+
+namespace Xamarin.Basics.Tests.Mvvm.Navigations.Controllers
+{
+    public partial class NavigationControllerTests
+    {
+        [Fact]
+        public void Can_Load_RootView()
+        {
+            // Arrange
+            Mock<IRootView> rootView = A.RootView;
+
+            var controller = new NavigationController();
+
+            // Act
+            controller.UseRootViewController(rootView.Object);
+
+            // Assert
+            rootView.Verify(m => m.Load(), Times.Once);
+        }
+        
+        [Fact]
+        public void Can_Load_StackRootView()
+        {
+            // Arrange
+            Mock<IStackView> rootStackView = A.StackView;
+
+            var controller = new NavigationController();
+
+            // Act
+            controller.UseStackRootViewController(rootStackView.Object);
+
+            // Assert
+            rootStackView.Verify(m => m.Load(), Times.Once);
+        }
+    }
+}
