aea393fd437998435bd995e18341332d94379a26
Bruno Moureau <bruno.moureau@hotmail.com>
2022-07-28 08:25:58 +0200

    removed fonts and imports empty resources folders to csproj

--- src-maui/MAUITemplate/src/MAUI.Template/MauiProgram.cs
+++ src-maui/MAUITemplate/src/MAUI.Template/MauiProgram.cs
@@ -5,13 +5,9 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
+
 		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			.UseMauiApp<App>();
 
 		return builder.Build();
 	}
